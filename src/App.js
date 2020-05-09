5892bf092aade9a76ab4a99a4759920cdecbdcc3
Vinicius Canuto <vcanutodev@gmail.com>
2020-05-09 12:09:37 -0300

    Resolução do desafio, adicionar e remover repos

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from "react";
-
+import React,{useState, useEffect} from "react";
+import api from "./services/api";
 import "./styles.css";
 
+
+ 
 function App() {
-  async function handleAddRepository() {
-    // TODO
+  const [repositories,setRepositories] =useState([]);
+
+  useEffect(()=>{
+    api.get('repositories').then(response =>{
+      setRepositories(response.data);
+    })
+  }, []);
+
+  
+
+  async function handleAddRepository(id, title, url, techs) {
+    api.post('repositories',{id,title,url,techs}).then(response =>{
+      setRepositories([...repositories, response.data]);
+    })
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete(`repositories/${id}`).then(()=>{
+      const filteredRepositories = repositories.filter(repository => repository.id != id);
+      setRepositories(filteredRepositories);
+    });
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository =>(
+          <>
+            <li key={repository.id}> 
+              {repository.title}
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>
+          </>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
